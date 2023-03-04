8dbb38b3f822e882bc991e725a6367a2ce883747
jaybharadia <jay.p.bharadia@gmail.com>
2023-03-04 18:38:50 +0530

    Added vue timer

---
+++ src/components/Timer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+      <span>{{ label }} : </span>
+      <span v-if="timer.days > 0">{{ timer.days }} : </span
+      ><span v-if="timer.hours > 0">{{ timer.hours }} : </span
+      ><span>{{ timer.minutes }}m : </span><span>{{ timer.seconds }}s</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useTimer } from 'vue-timer-hook';
+import { onMounted, watchEffect } from 'vue';
+export default {
+  emits: ['timeup'],
+  setup(props, { emit }) {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + props.seconds); // 10 minutes timer
+
+    const timer = useTimer(time);
+
+    onMounted(() => {
+      watchEffect(async () => {
+        if (timer.isExpired.value) {
+          console.warn('IsExpired');
+          emit('timeup');
+        }
+      });
+    });
+
+    return {
+      timer
+    };
+  },
+
+  props: {
+    seconds: {
+      type: Number,
+      required: true
+    },
+    label: {
+      type: String,
+      default: 'Timer : '
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
