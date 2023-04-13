cd7f05f3a04ccc49325a08275264b4a2a00dca4b
jaybharadia <jay.p.bharadia@gmail.com>
2023-04-13 09:54:14 +0530

    Removed password toggle input from global js

--- src/boot/global.js
+++ src/boot/global.js
@@ -1,8 +1,6 @@
-import { boot } from 'quasar/wrappers'
+import { boot } from 'quasar/wrappers';
 
-import PasswordToggleInput from 'src/components/form/inputs/PasswordToggleInput.vue'
-import PrimaryButton from 'src/components/buttons/Primary.vue'
+import PrimaryButton from 'src/components/buttons/Primary.vue';
 export default boot(({ app }) => {
-  app.component('PasswordToggleInput', PasswordToggleInput)
-  app.component('PrimaryButton', PrimaryButton)
-})
+  app.component('PrimaryButton', PrimaryButton);
+});
